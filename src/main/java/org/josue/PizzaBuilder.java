f64927b532b95485147d5a098fd46076fabcfd97
Josue Morales <josuemral10@gmail.com>
2021-10-16 10:47:17 -0600

    Segundo cambio

---
+++ src/main/java/org/josue/PizzaBuilder.java
@@ -0,0 +1,19 @@
+package org.josue;
+
+public class PizzaBuilder {
+    private PizzaBuilder pizzaBuilder;
+    public void setPizzaBuilder(PizzaBuilder pb) {
+        pizzaBuilder = pb;
+    }
+    public Pizza getPizza() {
+        return pizzaBuilder.getPizza();
+    }
+    public void crearPizza() {
+        pizzaBuilder.crearNuevaPizza();
+        pizzaBuilder.ponerNombre();
+        pizzaBuilder.crearMasa();
+        pizzaBuilder.crearSalsa();
+        pizzaBuilder.crearIngredientes();
+    }
+}
+}
