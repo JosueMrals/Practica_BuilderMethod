0f9bd44df0e0ed9df8a3a8af7485911c090494a0
Josue Morales <josuemral10@gmail.com>
2021-10-16 11:23:23 -0600

    Agregando la clase Cocinero como director del patron de diseno

---
+++ src/main/java/org/josue/Cocinero.java
@@ -0,0 +1,24 @@
+package org.josue;
+
+public class Cocinero {
+    private PizzaBuilder pizzaBuilder;
+    public void setPizzaBuilder(PizzaBuilder pb)
+    {
+        pizzaBuilder = pb;
+    }
+
+    public Pizza getPizza()
+    {
+        return pizzaBuilder.getPizza();
+    }
+
+    public void crearPizza()
+    {
+        pizzaBuilder.crearNuevaPizza();
+        pizzaBuilder.ponerNombre();
+        pizzaBuilder.crearMasa();
+        pizzaBuilder.crearSalsa();
+        pizzaBuilder.crearIngredientes();
+    }
+
+}
